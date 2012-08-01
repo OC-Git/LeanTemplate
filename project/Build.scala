f4a274911e3adfedfe1818ac9a2ed879eb389c12
Sebastian Baltes <baltes@objectcode.de>
2012-08-01 18:15:43 +0200

    Modules

--- project/Build.scala
+++ project/Build.scala
@@ -39,7 +39,7 @@ object ApplicationBuild extends Build {
 	val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
 	  // Add your own project settings here
 	    lessEntryPoints <<= baseDirectory(customLessEntryPoints),
-	    resolvers += Resolver.url("Sebastian Baltes Repository", url("http://sebastianbaltesobjectcode.github.com/releases"))(Resolver.ivyStylePatterns)
+	    resolvers += Resolver.url("Sebastian Baltes Repository", url("http://sebastianbaltesobjectcode.github.com/releases/"))(Resolver.ivyStylePatterns)
 	)    
 
 }
