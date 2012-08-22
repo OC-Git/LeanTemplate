71b6246a26ec2be69a0a87b0a1a34c7d2c5975cc
greenteafreak <user@localhorst>
2012-08-22 20:30:43 +0200

    next

--- app/controllers/admin/MonitoringController.java
+++ app/controllers/admin/MonitoringController.java
@@ -110,15 +110,93 @@ public class MonitoringController extends Controller {
 	}
 	
 	public static Result statsJson(final String from, final String to, final String resolutionName, final String byNode) throws IOException {
-		
-		// get form data: 
-		// from-date, to-date, resolution
-		// for single node ? or all ? 
-		
+		return stats(from, to, resolutionName, byNode, 				
+				"sum(request_count) as request_count, " +
+				"avg(response_time_avg) as response_time_avg, " +
+				"sum(exceptions_sum) as exceptions_sum, " +
+				"avg(db_connections_open) as db_connections_open, " +
+				"avg(db_connections_leased) as db_connections_leased, " +
+				"avg(heap_used) as heap_used, " +
+				"avg(heap_max) as heap_max, " +
+				"avg(heap_free) as heap_free, " +
+				"avg(swap_used) as swap_used, " +
+				"sum(gc_count) as gc_count, " +
+				"sum(gc_time_avg) as gc_time_avg, " +
+				"avg(load_avg) as load_avg, " +
+				"avg(thread_count) as thread_count " +
+				"from monitor_fine ", null, new JsonPopulator() {
+
+			@Override
+			public void call(ObjectNode node, SqlRow row, boolean groupByNodes) {
+				final String printTimestamp = row.getString("timestamp");
+				if (groupByNodes) node.put("nodeId", row.getString("node_id"));
+				
+				node.put("timestamp", printTimestamp);
+				node.put("timestampMillis", fillTimestamp(printTimestamp).getTime());
+				node.put("requestCount", row.getLong("request_count"));
+				node.put("responseTimeAvg", row.getLong("response_time_avg"));
+				node.put("exceptionsSum", row.getLong("exceptions_sum"));
+				node.put("dbConnectionsOpen", row.getLong("db_connections_open"));
+				node.put("dbConnectionsLeased", row.getLong("db_connections_leased"));
+				node.put("heapUsed", row.getLong("heap_used"));
+				node.put("heapMax", row.getLong("heap_max"));
+				node.put("heapFree", row.getLong("heap_free"));
+				node.put("swapUsed", row.getLong("swap_used"));
+				node.put("gcCount", row.getLong("gc_count"));
+				node.put("gcTimeAvg", row.getLong("gc_time_avg"));
+				node.put("loadAvg", row.getLong("load_avg"));
+				node.put("threadCount", row.getLong("thread_count"));
+			}
+		});
+	}
+
+	public static Result statsByRequestJson(final String from, final String to, final String resolutionName, final String byNode) throws IOException {
+		return stats(from, to, resolutionName, byNode, 
+				"request_method as request_method, " +
+				"sum(request_count) as request_count, " +
+				"avg(response_time) as response_time " +
+				"from monitor_response_time_fine ", "request_method", new JsonPopulator() {
+
+			@Override
+			public void call(ObjectNode node, SqlRow row, boolean groupByNodes) {
+				final String printTimestamp = row.getString("timestamp");
+				if (groupByNodes) node.put("nodeId", row.getString("node_id"));
+				
+				node.put("timestamp", printTimestamp);
+				node.put("timestampMillis", fillTimestamp(printTimestamp).getTime());
+				node.put("requestMethod", row.getString("request_method"));
+				node.put("requestCount", row.getLong("request_count"));
+				node.put("responseTime", row.getLong("response_time"));
+			}
+		});
+	}
+	
+	public static Result statsByExceptionJson(final String from, final String to, final String resolutionName, final String byNode) throws IOException {
+		return stats(from, to, resolutionName, byNode, 
+				"exception_type as exception_type, " +
+				"sum(exceptions_sum) as exceptions_sum " +
+				"from monitor_exceptions_fine ", "exception_type", new JsonPopulator() {
+
+			@Override
+			public void call(ObjectNode node, SqlRow row, boolean groupByNodes) {
+				final String printTimestamp = row.getString("timestamp");
+				if (groupByNodes) node.put("nodeId", row.getString("node_id"));
+				
+				node.put("timestamp", printTimestamp);
+				node.put("timestampMillis", fillTimestamp(printTimestamp).getTime());
+				node.put("exceptionType", row.getString("exception_type"));
+				node.put("exceptionsSum", row.getLong("exceptions_sum"));
+			}
+		});
+	}
+	
+	private static Result stats(final String from, final String to, final String resolutionName, final String byNode,
+		final String queryPart, final String extraGroupBy, final JsonPopulator callback) throws IOException {
+	
 		final Timestamp fromTs;
 		final Timestamp toTs;
 		final boolean groupByNodes = byNode != null && byNode.equals("1");
-		
+
 		try {
 			fromTs = new Timestamp(Long.parseLong(from));
 			toTs = new Timestamp(Long.parseLong(to));
@@ -140,56 +218,113 @@ public class MonitoringController extends Controller {
 		
 		bf.append("select ").append(groupBy).append(" as timestamp,");
 		if (groupByNodes) bf.append("node_id,");
-		bf.append(
-				"sum(request_count) as request_count, " +
-				"avg(response_time_avg) as response_time_avg, " +
-				"sum(exceptions_sum) as exceptions_sum, " +
-				"avg(db_connections_open) as db_connections_open, " +
-				"avg(db_connections_leased) as db_connections_leased, " +
-				"avg(heap_used) as heap_used, " +
-				"avg(heap_max) as heap_max, " +
-				"avg(heap_free) as heap_free, " +
-				"avg(swap_used) as swap_used, " +
-				"sum(gc_count) as gc_count, " +
-				"sum(gc_time_avg) as gc_time_avg, " +
-				"avg(load_avg) as load_avg, " +
-				"avg(thread_count) as thread_count " +
-				"from monitor_fine " +
-				"where timestamp >= :from_date and timestamp  <= :to_date " + 
-				"group by ").append(groupBy);
-
+		bf.append(queryPart);
+		bf.append( "where timestamp >= :from_date and timestamp  <= :to_date group by ").append(groupBy);
+		
+		if (extraGroupBy != null) bf.append(", ").append(extraGroupBy);
+		
 		if (groupByNodes) bf.append(", node_id");
 		bf.append(" order by ").append(groupBy);
-
+		
 		final List<SqlRow> rowList = Ebean.createSqlQuery(bf.toString()).setParameter("from_date", fromTs)
 				.setParameter("to_date", toTs).findList();
 
 		final ArrayNode nodeList = Json.newObject().arrayNode();
+		
 		for (final SqlRow row : rowList) {
 			final ObjectNode node = Json.newObject();
-			final String printTimestamp = row.getString("timestamp");
-
-			if (groupByNodes) node.put("nodeId", row.getString("node_id"));
-			node.put("timestamp", printTimestamp);
-			node.put("timestampMillis", fillTimestamp(printTimestamp).getTime());
-			node.put("requestCount", row.getLong("request_count"));
-			node.put("responseTimeAvg", row.getLong("response_time_avg"));
-			node.put("exceptionsSum", row.getLong("exceptions_sum"));
-			node.put("dbConnectionsOpen", row.getLong("db_connections_open"));
-			node.put("dbConnectionsLeased", row.getLong("db_connections_leased"));
-			node.put("heapUsed", row.getLong("heap_used"));
-			node.put("heapMax", row.getLong("heap_max"));
-			node.put("heapFree", row.getLong("heap_free"));
-			node.put("swapUsed", row.getLong("swap_used"));
-			node.put("gcCount", row.getLong("gc_count"));
-			node.put("gcTimeAvg", row.getLong("gc_time_avg"));
-			node.put("loadAvg", row.getLong("load_avg"));
-			node.put("threadCount", row.getLong("thread_count"));
+			callback.call(node, row, groupByNodes);
 			nodeList.add(node);
 		}
-
 		return deflateJson(nodeList.toString());
 	}
+	
+	private static interface JsonPopulator {
+		void call(ObjectNode node, SqlRow row, boolean groupByNodes);
+	}
+	
+	
+//	public static Result statsJson(final String from, final String to, final String resolutionName, final String byNode) throws IOException {
+//		
+//		// get form data: 
+//		// from-date, to-date, resolution
+//		// for single node ? or all ? 
+//		
+//		final Timestamp fromTs;
+//		final Timestamp toTs;
+//		final boolean groupByNodes = byNode != null && byNode.equals("1");
+//		
+//		try {
+//			fromTs = new Timestamp(Long.parseLong(from));
+//			toTs = new Timestamp(Long.parseLong(to));
+//		}
+//		catch(Exception e) {
+//			return badRequest("invalid date format");
+//		}
+//
+//		final MonitorResolution resolution;
+//		try {
+//			resolution = MonitorResolution.valueOf(resolutionName);
+//		}
+//		catch(final IllegalArgumentException e) {
+//			return badRequest("invalid resolution");
+//		}
+//		
+//		final StringBuilder bf = new StringBuilder(512);
+//		final String groupBy = "to_char(timestamp, '" + resolution.getPattern() + "')";
+//		
+//		bf.append("select ").append(groupBy).append(" as timestamp,");
+//		if (groupByNodes) bf.append("node_id,");
+//		bf.append(
+//				"sum(request_count) as request_count, " +
+//				"avg(response_time_avg) as response_time_avg, " +
+//				"sum(exceptions_sum) as exceptions_sum, " +
+//				"avg(db_connections_open) as db_connections_open, " +
+//				"avg(db_connections_leased) as db_connections_leased, " +
+//				"avg(heap_used) as heap_used, " +
+//				"avg(heap_max) as heap_max, " +
+//				"avg(heap_free) as heap_free, " +
+//				"avg(swap_used) as swap_used, " +
+//				"sum(gc_count) as gc_count, " +
+//				"sum(gc_time_avg) as gc_time_avg, " +
+//				"avg(load_avg) as load_avg, " +
+//				"avg(thread_count) as thread_count " +
+//				"from monitor_fine " +
+//				"where timestamp >= :from_date and timestamp  <= :to_date " + 
+//				"group by ").append(groupBy);
+//
+//		if (groupByNodes) bf.append(", node_id");
+//		bf.append(" order by ").append(groupBy);
+//
+//		final List<SqlRow> rowList = Ebean.createSqlQuery(bf.toString()).setParameter("from_date", fromTs)
+//				.setParameter("to_date", toTs).findList();
+//
+//		final ArrayNode nodeList = Json.newObject().arrayNode();
+//		for (final SqlRow row : rowList) {
+//			final ObjectNode node = Json.newObject();
+//			final String printTimestamp = row.getString("timestamp");
+//
+//			if (groupByNodes) node.put("nodeId", row.getString("node_id"));
+//			node.put("timestamp", printTimestamp);
+//			node.put("timestampMillis", fillTimestamp(printTimestamp).getTime());
+//			node.put("requestCount", row.getLong("request_count"));
+//			node.put("responseTimeAvg", row.getLong("response_time_avg"));
+//			node.put("exceptionsSum", row.getLong("exceptions_sum"));
+//			node.put("dbConnectionsOpen", row.getLong("db_connections_open"));
+//			node.put("dbConnectionsLeased", row.getLong("db_connections_leased"));
+//			node.put("heapUsed", row.getLong("heap_used"));
+//			node.put("heapMax", row.getLong("heap_max"));
+//			node.put("heapFree", row.getLong("heap_free"));
+//			node.put("swapUsed", row.getLong("swap_used"));
+//			node.put("gcCount", row.getLong("gc_count"));
+//			node.put("gcTimeAvg", row.getLong("gc_time_avg"));
+//			node.put("loadAvg", row.getLong("load_avg"));
+//			node.put("threadCount", row.getLong("thread_count"));
+//			nodeList.add(node);
+//		}
+//
+//		return deflateJson(nodeList.toString());
+//	}
 	
 	private static Result deflateJson(final String jsonText) throws IOException {
 		final Request request = request();
